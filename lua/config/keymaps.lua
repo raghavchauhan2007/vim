15e61c18eb01d487360ebfd6aa1e0d0f098c0249
Raghav Chauhan <chauhan.raghav2020@gmail.com>
2026-07-01 00:27:30 +0530

    add lsp keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,3 +5,19 @@ vim.keymap.set("n", "<leader>r", ":make <CR>")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'LSP Actions',
+  callback = function(event)
+    local opts = { buffer = event.buf }
+
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<leader>f', function()
+      vim.lsp.buf.format({ async = true })
+    end, opts)
+  end,
+})
