afd1429775da34f8e573ce5577456f955502b603
Raghav Chauhan <chauhan.raghav2020@gmail.com>
2026-07-20 17:27:07 +0530

    add git integration

---
+++ lua/plugins/git.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    'lewis6991/gitsigns.nvim',
+
+    config = function()
+      require('gitsigns').setup()
+    end
+  },
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    }
+  }
+}
