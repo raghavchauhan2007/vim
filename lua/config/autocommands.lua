98e75174f64a3b32a7a593b3f2e073bb49bda580
Raghav Chauhan <chauhan.raghav2020@gmail.com>
2026-08-19 00:33:30 +0530

    autodetect build.sh or makefile

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -6,7 +6,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
     vim.keymap.set('n', '<leader>f', function()
       vim.lsp.buf.format({ async = true })
@@ -18,17 +18,19 @@ local makeprg = vim.api.nvim_create_augroup('makeprg Configs', { clear = true })
 
 vim.api.nvim_create_autocmd('FileType', {
   group = makeprg,
-  pattern = 'c',
+  pattern = { 'c', 'cpp' },
   callback = function()
-    vim.opt_local.makeprg = 'clang -Wall -Wextra % -o %:r'
-  end,
-})
-
-vim.api.nvim_create_autocmd('FileType', {
-  group = makeprg,
-  pattern = 'cpp',
-  callback = function()
-    vim.opt_local.makeprg = 'clang++ -Wall -Wextra % -o %:r'
+    if vim.fn.filereadable('Makefile') == 1 then
+      vim.opt_local.makeprg = 'make'
+    elseif vim.fn.filereadable('build.sh') == 1 then
+      vim.opt_local.makeprg = './build.sh'
+    else
+      if vim.bo.filetype == 'cpp' then
+        vim.opt_local.makeprg = 'clang++ -Wall -Wextra % -o %:r'
+      else
+        vim.opt_local.makeprg = 'clang -Wall -Wextra % -o %:r'
+      end
+    end
   end,
 })
 
